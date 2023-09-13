bcb34aa07548e421044892fc25131817683c3141
Jaume <sidjaume@gmail.com>
2023-09-13 11:37:34 +0200

    add pic_nsfw endpoint

--- api.py
+++ api.py
@@ -66,35 +66,55 @@ def images_generator():
         return render_template('pic_gen.html', output = output )
     return render_template('pic_gen.html', output = 'Escribe un texto para crear una imagen')
 
+@app.route("/img_det", methods=["GET", "POST"])
+def img_nsfw():
+    import requests
+    if request.method == "POST":
+        # Obtener el archivo de imagen desde el formulario
+        img = request.files['image']
+        print(img)
+        # Verificar si se seleccionó un archivo
+        if img:
+            # Crear una solicitud POST con el archivo de imagen
+            url = "https://nsfw-images-detection-and-classification.p.rapidapi.com/adult-content-file"
+
+            # El encabezado Content-Type se configurará automáticamente con 'multipart/form-data'
+
+            # Crea un diccionario para contener los datos del formulario y la imagen
+            data = {}
+            data['image'] = (img.filename, img, img.content_type)
+
+            # Agrega los encabezados necesarios
+            headers = {
+                "X-RapidAPI-Key": "c679110cc2msh7f219734fbb8848p15052ejsnd747bb937243",
+                "X-RapidAPI-Host": "nsfw-images-detection-and-classification.p.rapidapi.com"
+            }
+
+            # Realiza la solicitud POST con los datos y encabezados
+            response = requests.post(url, files=data, headers=headers)
+            
+            # Meter todo en base d datos:
+
+            cols = {
+                'user':'-',
+                'img': data,
+                'response':str(response.json()['objects']),
+                'unsafe':str(response.json()['unsafe'])
+            }
+            index = [int(datetime.now().timestamp())]
+            df = pd.DataFrame(cols, index= index)
+            df.to_sql(name="pic_control",if_exists='append',con=engine, index = False)
+            
+            
+            return jsonify(response.json())
+        return render_template('img_form.html')
+
+    return render_template('img_form.html')
+
+@app.route('/get_pics', methods=["GET"])
+def get_logs():
+    return jsonify(pd.read_sql_query("select * from pic_control", con = engine).to_dict("records"))
+
 
 if __name__=="__main__":
     app.run(debug=True, port=8000)
-
-
-# import streamlit as st
-# import replicate
-# import os
-# replicate_api = 'r8_5x6EJXbsnYzL9aERZqTOhMbYQpKP2QO0o4C9e'
-# os.environ['REPLICATE_API_TOKEN'] = replicate_api
-# def generate_llama2_response(prompt_input):
-#     string_dialogue = "Tu función principal como analizador de lenguaje inapropiado es examinar un texto y determinar si contiene palabras o expresiones ofensivas. Si detectas lenguaje inapropiado, tu respuesta debe ser 'SÍ'. Si no detectas lenguaje inapropiado, tu respuesta debe ser 'NO'."
-#     output = replicate.run('meta/llama-2-70b-chat:35042c9a33ac8fd5e29e27fb3197f33aa483f72c2ce3b0b9d201155c7fd2a287',
-#                            input={"prompt": prompt_input,
-#                                   "system_prompt":string_dialogue})
-#     return output
-
-
-# prompt_input = ""
-
-# response = generate_llama2_response(prompt_input)
-# full_response = ''
-# placeholder = st.empty()
-
-# for item in response:
-#     full_response += item
-#     placeholder.markdown(full_response)
-# placeholder.markdown(full_response)
-# message = {"role": "Assistant", "content": full_response}
-
-
-# print(message['content'])
